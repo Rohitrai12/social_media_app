41965c80d0cee8ab8177a3bd44497155e9b6ae7a
Nurassyl <nmedeu@bu.edu>
2024-02-19 13:10:46 -0500

    fixed home image

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -32,30 +32,31 @@ const Home = () => {
   }
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full items-center">
       <div className="home-img-container">
-          <img
+        <img
           className="w-full h-auto object-cover transition-all duration-200 ease-in-out" /* This will ensure the image covers the container, is fully responsive, and maintains its aspect ratio */
-          src={"/assets/images/home-img.png"}
+          src={"/assets/images/home-img.jpg"}
           alt="Descriptive alt text for the image"
-        />
+          /> 
       </div>
 
-      <div className="home-posts-container">
-
-        <div className="home-posts">
-          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? (
-            <Loader />
-          ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full ">
-              {posts?.documents.map((post: Models.Document) => (
-                <li key={post.$id} className="flex justify-center w-full">
-                  <PostCard post={post} />
-                </li>
-              ))}
-            </ul>
-          )}
+      <div className="home-posts-container-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+        <div className="home-posts-container">
+          <div className="home-posts">
+            {isPostLoading && !posts ? (
+              <Loader />
+            ) : (
+              <ul className="flex flex-col flex-1 gap-9 w-full ">
+                {posts?.documents.map((post: Models.Document) => (
+                  <li key={post.$id} className="flex justify-center w-full">
+                    <PostCard post={post} />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </div>
